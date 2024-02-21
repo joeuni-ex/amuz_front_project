1942284a98ef7ccb5eb5f79d75c09127c23c7013
joeuni <dmsdl8917@gmail.com>
2024-02-21 21:02:43 +0900

    footer 완성

--- src/App.vue
+++ src/App.vue
@@ -187,13 +187,35 @@
     </div>
   </div>
   <!-- footer -->
-  <footer class="w-screen h-72 flex flex-1 flex-col bg-footerBg">
-    <div class="border-b border-gray-500"></div>
-    <div></div>
+  <footer
+    class="w-screen h-72 flex flex-1 flex-col bg-footerBg border-t border-gray-600"
+  >
+    <div
+      class="h-2/5 flex justify-center items-center border-b border-gray-600"
+    >
+      <div class="flex items-center w-3/5 justify-between">
+        <div class="w-36">
+          <LogoIcon />
+        </div>
+        <div class="w-3/5 text-center text-gray-400 space-x-11">
+          <span>메세지</span>
+          <span>크레딧</span>
+        </div>
+      </div>
+    </div>
+    <div class="h-3/5 flex justify-center">
+      <div class="w-3/5">
+        <p class="text-gray-400 mt-4">
+          문의 사항이 있으신가요? 텔레그램을 통해 고객센터로 문의 해 보세요.
+        </p>
+        <p class="text-gray-300 mt-3">elegram : @shdevelope</p>
+      </div>
+    </div>
   </footer>
 </template>
 <script>
 import LogoIcon from "./assets/logo.svg";
+import LogoIconGray from "./assets/logo-gray.svg";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 
 export default {
@@ -203,6 +225,7 @@ export default {
   setup() {
     const scrolled = ref(false);
 
+    // 스크롤 위치에 따라 헤더 색상 변경
     const handleScroll = () => {
       scrolled.value = window.scrollY > 0;
     };
