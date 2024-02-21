fa156f6e13e641cf6d8c9844f913a184bda67387
joeuni <dmsdl8917@gmail.com>
2024-02-22 00:09:30 +0900

    Footer 컴포넌트 분리 및 적용

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer
+    class="w-screen h-72 flex flex-1 flex-col bg-footerBg border-t border-gray-600"
+  >
+    <div
+      class="h-2/5 flex justify-center items-center border-b border-gray-600"
+    >
+      <div class="flex items-center w-3/5 justify-between">
+        <div class="w-36">
+          <LogoIcon />
+        </div>
+        <div class="w-3/5 text-center text-gray-400 space-x-11">
+          <span>메세지</span>
+          <span>크레딧</span>
+        </div>
+      </div>
+    </div>
+    <div class="h-3/5 flex justify-center">
+      <div class="w-3/5">
+        <p class="text-gray-400 mt-4">
+          문의 사항이 있으신가요? 텔레그램을 통해 고객센터로 문의 해 보세요.
+        </p>
+        <p class="text-gray-300 mt-3">elegram : @shdevelope</p>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+import LogoIcon from "../assets/logo.svg";
+
+export default {
+  components: {
+    LogoIcon,
+  },
+};
+</script>
+<style></style>
