98070ad0ed975f8c485ec74ad08a5fc265783e44
joeuni <dmsdl8917@gmail.com>
2024-02-22 21:17:35 +0900

    크레딧 페이지 완료

--- src/pages/Credit.vue
+++ src/pages/Credit.vue
@@ -1,7 +1,157 @@
-<template lang="">
-  <div></div>
+<template>
+  <div
+    class="w-screen h-screen bg-pageBgGray flex flex-1 flex-col items-center pt-20"
+  >
+    <div class="flex h-full justify-center items-center flex-col space-y-12">
+      <div class="flex flex-col space-y-5 text-center">
+        <p class="text-4xl font-PretendardBold">크레딧 구매</p>
+        <span class="text-xl text-gray-500"
+          >메시지 전송을 위해 사용할 크레딧 상품을 선택해 주세요</span
+        >
+      </div>
+      <div class="flex space-x-7">
+        <!-- 1 -->
+        <div class="flex flex-col justify-between credit-content p-8">
+          <p class="text-3xl font-PretendardBold">30,000 원</p>
+          <div>
+            <p class="text-lg text-gray-500">총 66,667건 발송가능합니다</p>
+            <p class="text-lg text-gray-500">1건당 15원에 결제가능합니다</p>
+          </div>
+          <div class="flex justify-between">
+            <p class="text-2xl font-PretendardSemiBold">330,000 크레딧</p>
+            <div
+              class="flex justify-center items-center font-PretendardSemiBold w-16 h-8 rounded-2xl bg-lightBlue text-darkPurple"
+            >
+              + 10%
+            </div>
+          </div>
+
+          <div class="p-4 bg-lightGray2 h-20">
+            <div class="flex justify-between w-full">
+              <div>기본 크레딧</div>
+              <div>300,000</div>
+            </div>
+            <div class="flex justify-between w-full">
+              <div>추가 크레딧</div>
+              <div>+ 30,000</div>
+            </div>
+          </div>
+          <button
+            class="w-full h-16 bg-buttonPurple2 rounded-lg my-2 text-xl text-white"
+          >
+            구매하기
+          </button>
+        </div>
+        <!-- 2 -->
+        <div class="flex flex-col justify-between credit-content p-8">
+          <p class="text-3xl font-PretendardBold">30,000 원</p>
+          <div>
+            <p class="text-lg text-gray-500">총 66,667건 발송가능합니다</p>
+            <p class="text-lg text-gray-500">1건당 15원에 결제가능합니다</p>
+          </div>
+          <div class="flex justify-between">
+            <p class="text-2xl font-PretendardSemiBold">330,000 크레딧</p>
+            <div
+              class="flex justify-center items-center font-PretendardSemiBold w-16 h-8 rounded-2xl bg-lightBlue text-darkPurple"
+            >
+              + 10%
+            </div>
+          </div>
+
+          <div class="p-4 bg-lightGray2 h-20">
+            <div class="flex justify-between w-full">
+              <div>기본 크레딧</div>
+              <div>300,000</div>
+            </div>
+            <div class="flex justify-between w-full">
+              <div>추가 크레딧</div>
+              <div>+ 30,000</div>
+            </div>
+          </div>
+          <button
+            class="w-full h-16 bg-buttonPurple2 rounded-lg my-2 text-xl text-white"
+          >
+            구매하기
+          </button>
+        </div>
+        <!-- 3 -->
+        <div class="flex flex-col justify-between credit-content p-8">
+          <p class="text-3xl font-PretendardBold">30,000 원</p>
+          <div>
+            <p class="text-lg text-gray-500">총 66,667건 발송가능합니다</p>
+            <p class="text-lg text-gray-500">1건당 15원에 결제가능합니다</p>
+          </div>
+          <div class="flex justify-between">
+            <p class="text-2xl font-PretendardSemiBold">330,000 크레딧</p>
+            <div
+              class="flex justify-center items-center font-PretendardSemiBold w-16 h-8 rounded-2xl bg-lightBlue text-darkPurple"
+            >
+              + 10%
+            </div>
+          </div>
+
+          <div class="p-4 bg-lightGray2 h-20">
+            <div class="flex justify-between w-full">
+              <div>기본 크레딧</div>
+              <div>300,000</div>
+            </div>
+            <div class="flex justify-between w-full">
+              <div>추가 크레딧</div>
+              <div>+ 30,000</div>
+            </div>
+          </div>
+          <button
+            class="w-full h-16 bg-buttonPurple2 rounded-lg my-2 text-xl text-white"
+          >
+            구매하기
+          </button>
+        </div>
+        <!-- 4 -->
+        <div class="flex flex-col justify-between credit-content p-8">
+          <p class="text-3xl font-PretendardBold">30,000 원</p>
+          <div>
+            <p class="text-lg text-gray-500">총 66,667건 발송가능합니다</p>
+            <p class="text-lg text-gray-500">1건당 15원에 결제가능합니다</p>
+          </div>
+          <div class="flex justify-between">
+            <p class="text-2xl font-PretendardSemiBold">330,000 크레딧</p>
+            <div
+              class="flex justify-center items-center font-PretendardSemiBold w-16 h-8 rounded-2xl bg-lightBlue text-darkPurple"
+            >
+              + 10%
+            </div>
+          </div>
+
+          <div class="p-4 bg-lightGray2 h-20">
+            <div class="flex justify-between w-full">
+              <div>기본 크레딧</div>
+              <div>300,000</div>
+            </div>
+            <div class="flex justify-between w-full">
+              <div>추가 크레딧</div>
+              <div>+ 30,000</div>
+            </div>
+          </div>
+          <button
+            class="w-full h-16 bg-buttonPurple2 rounded-lg my-2 text-xl text-white"
+          >
+            구매하기
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
 </script>
-<style lang=""></style>
+<style>
+.credit-content {
+  width: 347px;
+  height: 459px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 10px 10px #00000008;
+  border-radius: 8px;
+  opacity: 1;
+}
+</style>
