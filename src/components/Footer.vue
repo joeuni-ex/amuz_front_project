903c999e8635a3851e1a84b1a8623f2808a49ea0
joeuni <dmsdl8917@gmail.com>
2024-02-24 18:13:44 +0900

    모바일 반응형 css 추가

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,18 +5,20 @@
     <div
       class="h-2/5 flex justify-center items-center border-b border-gray-600"
     >
-      <div class="flex items-center w-3/5 justify-between">
+      <div class="flex items-center w-4/5 md:w-3/5 justify-between">
         <div>
           <LogoIconGray />
         </div>
-        <div class="w-3/5 text-center text-gray-400 space-x-11">
+        <div
+          class="w-4/5 md:w-3/5 text-center text-xs md:text-sm text-gray-400 space-x-11"
+        >
           <span>메세지</span>
           <span>크레딧</span>
         </div>
       </div>
     </div>
     <div class="h-3/5 flex justify-center">
-      <div class="w-3/5">
+      <div class="w-4/5 md:w-3/5">
         <p class="text-gray-400 mt-4">
           문의 사항이 있으신가요? 텔레그램을 통해 고객센터로 문의 해 보세요.
         </p>
